6eb4375e632238a94101c8f821837d0a20f1ecac
nstanogias <nstanogias@gmail.com>
2024-11-26 23:26:38 +0200

    added tokenInput component

--- src/app/vault/[address]/page.tsx
+++ src/app/vault/[address]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LiquidityForm from "@/components/LiquidityForm";
 import { Button } from "@/components/ui/button";
 import { useVault } from "@/hooks/useVault";
 import { formatTokenBalance } from "@/lib/utils";
@@ -18,30 +19,28 @@ export default function Vault({
 
   if (loading) return <p>Loading vault data...</p>;
   if (error) return <p className="text-red-500">{error}</p>;
+  if (!vaultData) return <p>No vault data found</p>;
 
   return (
     <div className="p-6">
-      <h1 className="text-xl font-bold">Vault Details</h1>
+      <h1 className="text-xl font-bold">Vault Balance:</h1>
       <div className="mt-4">
         <p>
-          <strong>{vaultData?.token0.name}</strong>{" "}
-          {vaultData?.token0.balance
-            ? formatTokenBalance(
-                vaultData?.token0.balance,
-                vaultData.token0.decimals
-              ).toFixed(5)
-            : "N/A"}
+          <strong>{vaultData.token0.name}</strong>{" "}
+          {formatTokenBalance(
+            vaultData.token0.balance,
+            vaultData.token0.decimals
+          ).toFixed(5)}
         </p>
         <p>
           <strong>{vaultData?.token1.name}</strong>{" "}
-          {vaultData?.token1.balance
-            ? formatTokenBalance(
-                vaultData?.token1.balance,
-                vaultData.token1.decimals
-              ).toFixed(5)
-            : "N/A"}
+          {formatTokenBalance(
+            vaultData.token1.balance,
+            vaultData.token1.decimals
+          ).toFixed(5)}
         </p>
       </div>
+      <LiquidityForm vaultData={vaultData} />
       <Button
         onClick={() => router.push("/")}
         className="w-fit text-white bg-gray-900 hover:bg-gray-700"
---
+++ src/components/TokenInput.tsx
@@ -0,0 +1,58 @@
+import { Input } from "./ui/input";
+
+interface Props {
+  id: string;
+  value: string;
+  onChange: (value: string) => void;
+  tokenName: string;
+  balance: number;
+  onMaxClick: () => void;
+  hasError: boolean;
+}
+
+const TokenInput = ({
+  id,
+  value,
+  onChange,
+  tokenName,
+  balance,
+  onMaxClick,
+  hasError,
+}: Props) => {
+  return (
+    <div
+      className={`p-4 border rounded-lg ${
+        hasError ? "border-red-500" : "border-gray-300"
+      }`}
+    >
+      <div className="flex justify-between items-center mb-2">
+        <div className="flex items-center space-x-2">
+          <span className="text-sm font-medium border px-4 py-1 rounded-full bg-orange-200 border-orange-700">
+            {tokenName}
+          </span>
+        </div>
+        <div className="text-sm text-gray-500">
+          Balance: {balance.toFixed(8)}
+          <button
+            type="button"
+            onClick={onMaxClick}
+            className="ml-2 text-blue-500 hover:underline"
+          >
+            MAX
+          </button>
+        </div>
+      </div>
+
+      <Input
+        id={id}
+        type="text"
+        placeholder="0.00"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        className="w-full"
+      />
+    </div>
+  );
+};
+
+export default TokenInput;
